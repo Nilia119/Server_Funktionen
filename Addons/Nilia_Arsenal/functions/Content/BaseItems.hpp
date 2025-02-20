bb8882e2ed4c77137d42d5e5c0bd55f1bfb1dee1
Nilia119 <niklas.rieck@gmail.com>
2025-02-20 18:57:31 +0100

    Added Missing ACE/KAT Items

--- Addons/Nilia_Arsenal/functions/Content/BaseItems.hpp
+++ Addons/Nilia_Arsenal/functions/Content/BaseItems.hpp
@@ -26,8 +26,11 @@
 "kat_fentanyl","kat_etomidate","kat_flumazenil","kat_Pulseoximeter","kat_stethoscope","kat_crossPanel","kat_IV_16","kat_vacuum","kat_BVM","kat_oxygenTank_150","kat_oxygenTank_300",
 "kat_pocketBVM","ACE_fieldDressing","ACE_elasticBandage","ACE_tourniquet","ACE_packingBandage","ACE_quikclot","ACE_epinephrine","ACE_morphine","ACE_plasmaIV","ACE_plasmaIV_250",
 "ACE_plasmaIV_500","ACE_bodyBag_white","ACE_splint","ACE_adenosine","ACE_salineIV_250","ACE_salineIV_500","ACE_surgicalKit","ACE_salineIV","ACE_bodyBag_blue","ACE_ATNAA",
-"ACE_bodyBag","ACE_painkillers",
+"ACE_bodyBag","ACE_painkillers","kat_personal_oxygen",
 //  Basis Items
 "ACE_EarPlugs","EUFOR_Lexikon","ACE_Flashlight_XL50","ACE_SpraypaintBlack","ACE_SpraypaintRed","ACE_SpraypaintBlue","ACE_SpraypaintGreen","ACE_SpraypaintYellow","ACE_WaterBottle",
 "ACE_SpraypaintWhite","ACE_DefusalKit","ACE_MapTools","ItemcTabHCam","ACE_SpareBarrel","ACE_RangeCard","ACE_artilleryTable","ACE_Banana","ACE_RangeTable_82mm","ACE_rope6",
-"greenmag_item_speedloader","KNB_Notebook","KNB_PanelYellow","KNB_PanelGreen","KNB_PanelRed"
+"greenmag_item_speedloader","KNB_Notebook","KNB_PanelYellow","KNB_PanelGreen","KNB_PanelRed","acex_intelitems_notepad","ACE_PlottingBoard","ACE_Flashlight_MX991",
+"ACE_EHP","ace_flags_blue","ace_flags_yellow","ace_flags_green","ace_flags_orange","ace_flags_red","ace_flags_black","ace_flags_purple","ace_flags_white","tsp_sling",
+"ACE_Can_Franta","ACE_Can_RedGull","ACE_Can_Spirit","ACE_MRE_CreamChickenSoup","ACE_MRE_CreamTomatoSoup","ACE_MRE_MeatballsPasta","ACE_MRE_ChickenHerbDumplings",
+"ACE_MRE_ChickenTikkaMasala","ACE_MRE_LambCurry"
